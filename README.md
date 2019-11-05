3d8b1923e8e6ce020cc75b4b934c31f6c31a8976
caozhichongchong <caozhichongchong@gmail.com>
2019-11-05 14:42:39 -0500

    traits_finder as main programs for search and summarize

--- README.md
+++ README.md
@@ -15,26 +15,34 @@ in preparation: `anaconda download caozhichongchong/traits_finder`
 https://pypi.org/project/traits_finder
 
 ## How to use it
+### Search traits: boring and slow...
 1. search protein reference sequences in genomes by similarity search\
-`traits_finder -db your.db -i your.input.folder -fa your.input.genome.format --orf your.input.orf.format --r your.output.folder --r16 your.output.folder.for.16s --u diamond --bp blastp -dbf 1 -inf 1 -s 1`\
+`traits_finder genome -db your.db -i your.input.folder -fa your.input.genome.format --orf your.input.orf.format --r your.output.folder --r16 your.output.folder.for.16s --u diamond --bp blastp -dbf 1 -s 1`\
 
 2. search protein reference sequences in metagenomes by similarity search\
-`traits_finder -db your.db -i your.input.folder -fa your.input.metagenomes.format --r your.output.folder --r16 your.output.folder.for.16s --u diamond --bp blastp -dbf 1 -inf 2 -s 1`\
+`traits_finder meta -db your.db -i your.input.folder -fa your.input.metagenomes.format --r your.output.folder --r16 your.output.folder.for.16s --u diamond --bp blastp -dbf 1 -s 1`\
 
 3. search dna reference sequences in genomes by similarity search\
-`traits_finder -db your.db -i your.input.folder -fa your.input.genome.format --orf your.input.orf.format --r your.output.folder --r16 your.output.folder.for.16s --u usearch.or.hs-blastn --bp blastn -dbf 2 -inf 1 -s 1`\
+`traits_finder genome -db your.db -i your.input.folder -fa your.input.genome.format --orf your.input.orf.format --r your.output.folder --r16 your.output.folder.for.16s --u usearch.or.hs-blastn --bp blastn -dbf 2 -s 1`\
 
 4. search dba reference sequences in metagenomes by similarity search\
-`traits_finder -db your.db -i your.input.folder -fa your.input.metagenomes.format --r your.output.folder --r16 your.output.folder.for.16s --u usearch.or.hs-blastn --bp blastn -dbf 2 -inf 2 -s 1`\
+`traits_finder meta -db your.db -i your.input.folder -fa your.input.metagenomes.format --r your.output.folder --r16 your.output.folder.for.16s --u usearch.or.hs-blastn --bp blastn -dbf 2 -s 1`\
 
 5. search protein reference sequences in genomes by hmm\
-`traits_finder -db your.db -i your.input.folder -fa your.input.genome.format --orf your.input.orf.format --r your.output.folder --r16 your.output.folder.for.16s --hmm hmmscan -dbf 1 -inf 1 -s 2`\
+`traits_finder genome -db your.db -i your.input.folder -fa your.input.genome.format --orf your.input.orf.format --r your.output.folder --r16 your.output.folder.for.16s --hmm hmmscan -dbf 1 -s 2`\
 
 6. search dna reference sequences in genomes by alignment\
-`traits_finder -db your.db -i your.input.folder -fa your.input.genome.format --orf your.input.orf.format --r your.output.folder --r16 your.output.folder.for.16s --u usearch.or.hs-blastn --bp blastn --bwa bwa -dbf 2 -inf 1 -s 1`\
+`traits_finder genome -db your.db -i your.input.folder -fa your.input.genome.format --orf your.input.orf.format --r your.output.folder --r16 your.output.folder.for.16s --u usearch.or.hs-blastn --bp blastn --bwa bwa -dbf 2 -s 1`\
 
 7. search dna reference sequences in metagenomes by alignment\
-`traits_finder -db your.db -i your.input.folder -fa your.input.metagenomes.format --r your.output.folder --r16 your.output.folder.for.16s --u usearch.or.hs-blastn --bp blastn --bwa bwa -dbf 2 -inf 2 -s 1`\
+`traits_finder meta -db your.db -i your.input.folder -fa your.input.metagenomes.format --r your.output.folder --r16 your.output.folder.for.16s --u usearch.or.hs-blastn --bp blastn --bwa bwa -dbf 2 -s 1`\
+
+### Summarize results: cool and fast!
+1. summarize traits in genome\
+`traits_finder sum_genome -db your.db -m function.mapping.your.db -i your.input.folder -fa your.input.genome.format --orf your.input.orf.format --r your.output.folder --r16 your.output.folder.for.16s`\
+
+2. summarize traits in metagenomes\
+`traits_finder sum_meta -db your.db -m function.mapping.your.db -i your.input.folder -fa your.input.metagenomes.format --r your.output.folder --r16 your.output.folder.for.16s -s 1`\
 
 ## Results
 
--- traits_finder/__main__.py
+++ traits_finder/__main__.py
@@ -27,34 +27,40 @@ def main():
     )
     ############################################ Arguments and declarations ##############################################
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-db",
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+    required.add_argument('program',
+                        help="traits_finder genome for analyzing genomes, \
+                        traits_finder meta for analyzing metagenomes, \
+                        traits_finder sum_genome for summarizing genome results,\
+                        traits_finder sum_meta for summarizing metagenome results",
+                        type=str,
+                        default='genome',
+                        choices=['genome', 'meta','sum_genome','sum_meta'],
+                        action='store',
+                        metavar='traits_finder program')
+    required.add_argument("-db",
                         help="file name of your input database",
-                        type=str, default='Butyrate.pro.aa',metavar='database.aa')
-    parser.add_argument("-dbf",
+                        type=str, default='Butyrate.pro.aa',
+                        metavar='database.aa')
+    optional.add_argument("-dbf",
                         help="sequence format of your input database\
                         (1: nucleotide; 2: protein), \
                         (default \'1\' for nucleotide)",
                         metavar="1 or 2",
                         choices=[1, 2],
                         action='store', default=1, type=int)
-    parser.add_argument("-m",
+    required.add_argument("-m",
                         help="mapping file of traits to function", type=str,
                         default='Butyrate.pro.mapping.txt',
-                        metavar='Butyrate.pro.mapping.txt')
-    parser.add_argument("-i",
+                        metavar='database.mapping.txt')
+    required.add_argument("-i",
                         help="input folder of metagenomes or genomes", type=str,
                         default='.',metavar='current dir (.)')
-    parser.add_argument("-inf",
-                        help="input format\
-                        (1: genomes; 2: metagenomes), \
-                        (default \'1\' for genomes)",
-                        metavar="1 or 2",
-                        choices=[1, 2],
-                        action='store', default=1, type=int)
-    parser.add_argument("-fa",
+    required.add_argument("-fa",
                         help="input format of genome/metagenome sequence",
                         type=str, default='.fa', metavar='.fasta, .fna, .fastq or .fa')
-    parser.add_argument('-s',
+    optional.add_argument('-s',
                         help="set the method to search the your database \
                         (1: blast; 2: hmm), \
                         (default \'1\' for blast search)",
@@ -62,54 +68,54 @@ def main():
                         choices=[1, 2],
                         action='store', default=1, type=int)
     # optional parameters
-    parser.add_argument("--l",
+    optional.add_argument("--l",
                         help="input list of metagenomes", type=str,
                         default='None',metavar='rep_metagenomes.txt')
-    parser.add_argument("--meta",
+    optional.add_argument("--meta",
                         help="metadata  of metagenomes", type=str,
                         default='None',
                         metavar='metadata.metagenomes.txt')
-    parser.add_argument("--orf",
+    optional.add_argument("--orf",
                         help="input format of genomes orfs", type=str, default='.genes.faa',metavar='.faa')
     # optional output setup
-    parser.add_argument("--r",
+    optional.add_argument("--r",
                         help="output directory or folder of your results",
                         type=str, default='Result',metavar='Result')
-    parser.add_argument("--r16",
+    optional.add_argument("--r16",
                         help="output directory or folder of your 16S sequences",
                         type=str, default='Result',metavar='Result')
     # optional search parameters
-    parser.add_argument('--t',
+    optional.add_argument('--t',
                         help="Optional: set the thread number assigned for running XXX (default 1)",
                         metavar="1 or more", action='store', default=1, type=int)
-    parser.add_argument('--id',
+    optional.add_argument('--id',
                         default=75.0, action='store', type=float, metavar='60.0',
                         help='Optional: set the amno acid based identity cutoff for blast (default is 80.0)\n'
                              'Leave it alone if hmm is used')
-    parser.add_argument('--ht',
+    optional.add_argument('--ht',
                         default=75.0, action='store', type=float, metavar='60.0',
                         help='Optional: set the amno acid based hit-length cutoff for blast (default is 80.0)\n'
                              'Leave it alone if hmm is used')
-    parser.add_argument('--e',
+    optional.add_argument('--e',
                         default=1e-2, action='store', type=float, metavar='1e-5',
                         help='Optional: set the evalue cutoff for blast or hmm (default is 1e-5)')
     # requirement for software calling
-    parser.add_argument('--u',
+    optional.add_argument('--u',
                         help="Optional: use two-step method for blast search,"+
                              " \'None\' for using one step, \'usearch\' or \'diamond\' for using two-step \
                              (complete path to usearch or diamond if not in PATH, \
                              please make sure the search tools can be directly called), (default: \'None\')",
                         metavar="None or usearch",
                         action='store', default='None', type=str)
-    parser.add_argument('--hmm',
+    optional.add_argument('--hmm',
                         help="Optional: complete path to hmmscan if not in PATH,",
                         metavar="/usr/local/bin/hmmscan",
                         action='store', default='hmmscan', type=str)
-    parser.add_argument('--bp',
+    optional.add_argument('--bp',
                         help="Optional: complete path to blastp or blastn if not in PATH, \'None\' for no blast search",
                         metavar="/usr/local/bin/blastp",
                         action='store', default='blastp', type=str)
-    parser.add_argument('--bwa',
+    optional.add_argument('--bwa',
                         help="Optional: complete path to bwa if not in PATH,",
                         metavar="/usr/local/bin/bwa",
                         action='store', default='None', type=str)
@@ -118,21 +124,27 @@ def main():
     workingdir=os.path.abspath(os.path.dirname(__file__))
     ################################################### Programme #######################################################
     f1 = open ('traits_finder.log','w')
-    if args.inf == 1:
+    if args.program == 'genome':
         cmd = ('python '+workingdir+'/Traits_WG.py -db %s -dbf %s -i %s -s %s --fa %s --orf %s --r %s --r16 %s --t %s --id %s --ht %s --e %s --u %s --hmm %s --bp %s --bwa %s\n'
         % (str(args.db),str(args.dbf),str(args.i),str(args.s),str(args.fa),str(args.orf),str(args.r),str(args.r16),str(args.t),str(args.id),str(args.ht),str(args.e),str(args.u),str(args.hmm),str(args.bp),str(args.bwa)))
-        #cmd += ('python '+workingdir+'/scripts/Traits_summary_WG.py -t %s -db %s --fa %s --orf %s -i %s -m %s --r %s --r16 %s --s %s -c %s\n'
-        #%(str(os.path.split(args.db)[1]),str(args.db),str(args.fa),str(args.orf),str(args.i),str(args.m),str(args.r),str(args.r16),str(os.path.join(args.r,'summary')),str(args.id)))
         f1.write(cmd)
         os.system(cmd)
-    else:
+    elif args.program == 'meta':
         cmd = ('python '+workingdir+'/Traits_MG.py -db %s -dbf %s -i %s -s %s --fa %s -l %s --r %s --r16 %s --t %s --id %s --ht %s --e %s --u %s --hmm %s --bp %s --bwa %s\n'
         % (str(args.db),str(args.dbf),str(args.i),str(args.s),str(args.fa),str(args.l),str(args.r),str(args.r16),str(args.t),str(args.id),str(args.ht),str(args.e),str(args.u),str(args.hmm),str(args.bp),str(args.bwa)))
+        f1.write(cmd)
+        os.system(cmd)
+    elif args.program == 'sum_genome':
+        cmd = ('python '+workingdir+'/scripts/Traits_summary_WG.py -t %s -db %s --fa %s --orf %s -i %s -m %s --r %s --r16 %s --s %s -c %s\n'
+        %(str(os.path.split(args.db)[1]),str(args.db),str(args.fa),str(args.orf),str(args.i),str(args.m),str(args.r),str(args.r16),str(os.path.join(args.r,'summary')),str(args.id)))
+        f1.write(cmd)
+        os.system(cmd)
+    elif args.program == 'sum_meta':
         #if args.s == 1:
-        #   cmd += ('python '+workingdir+'/Copynumber_traits.py -i %s -i16 %s -o %s -f %s.blast.txt -f16 %s.16S.txt -l %s.length -tf %s -m %s -c %s'
+        #   cmd = ('python '+workingdir+'/Copynumber_traits.py -i %s -i16 %s -o %s -f %s.blast.txt -f16 %s.16S.txt -l %s.length -tf %s -m %s -c %s'
         #    % (str(os.path.join(args.r,'search_output/0'))),str(args.r16),str(os.path.join(args.r,'summary'))),str(args.fa),str(args.fa),str(args.db),str(args.m),str(args.meta),str(args.id))
         #else:
-            #cmd += ('python '+workingdir+'/Copynumber_traits_hmm.py -i %s -i16 %s -o %s -f %s.hmm2.txt -f16 %s.16S.txt -l %s.length -tf %s -m %s -c %s'
+            #cmd = ('python '+workingdir+'/Copynumber_traits_hmm.py -i %s -i16 %s -o %s -f %s.hmm2.txt -f16 %s.16S.txt -l %s.length -tf %s -m %s -c %s'
             #% (str(os.path.join(args.r,'search_output/0'))),str(args.r16),str(os.path.join(args.r,'summary'))),str(args.fa),str(args.fa),str(args.db),str(args.m),str(args.meta),str(args.e))
         #cmd += ('python '+workingdir+'/scripts/Traits_summary_MG.py -t %s -db %s --fa %s --orf %s -i %s -m %s --r %s --r16 %s --s %s -c %s\n'
         #%(str(os.path.split(args.db)[1]),str(args.db),str(args.fa),str(args.orf),str(args.i),str(args.m),str(args.r),str(args.r16),str(os.path.join(args.r,'summary')),str(args.id)))
